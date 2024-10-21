c05b755324ce81c686c2ed774928606252636bbb
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-10-20 21:31:40 -0300

    build: update docker compose

---
+++ .docker/data/sql/init.sql
@@ -0,0 +1,14 @@
+BEGIN
+CREATE DATABASE development;
+
+\connect development;
+
+CREATE SCHEMA IF NOT EXISTS auth;
+
+CREATE TABLE IF NOT EXISTS auth.users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+END;
