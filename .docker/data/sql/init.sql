634a2ff0cdcd89f3b0ca77348b2aeb97ffe44bec
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-10-25 13:41:06 -0300

    chore: criado volume administrado pelo docker daemon

--- .docker/data/sql/init.sql
+++ .docker/data/sql/init.sql
@@ -1,4 +1,3 @@
-BEGIN
 CREATE DATABASE development;
 \connect development;
 
@@ -10,4 +9,3 @@ CREATE TABLE IF NOT EXISTS auth.users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
-END;
