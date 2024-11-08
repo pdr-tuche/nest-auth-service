3f7b944f85dc9cf5d4ac362cce4eaef787a91ab4
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-11-07 22:48:15 -0300

    feat: criado configuracao do pipe de validacao

--- src/common/dtos/user/user-dto-post-request.dto.ts
+++ src/common/dtos/user/user-dto-post-request.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class UserDtoPostRequest {
+  @IsString()
+  @Length(3, 255)
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @Length(8, 30)
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigEnum } from './common/enums/app-config.enum';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
     }),
   );
   await app.listen(AppConfigEnum.APP_PORT ?? 3000);
