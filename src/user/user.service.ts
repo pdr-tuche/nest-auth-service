f09e503356fe8a6335c4e755d71c2bab04559cfc
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-11-10 22:30:39 -0300

    refactor: criado metodos de apoio e interface base do servico

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,26 +7,13 @@ import { UserNotFoundException } from 'src/common/exceptions/user-not-found.exce
 import { InvalidEmailException } from 'src/common/exceptions/invalid-email.execption';
 import { UserDtoPutRequest } from '../common/dtos/user/user-dto-put-request.dto';
 import { AppConfigEnum } from 'src/common/enums/app-config.enum';
+import { UserServiceInterface } from './user-service.interface';
 
 @Injectable()
-export class UserService {
+export class UserService implements UserServiceInterface {
   constructor(private prismaService: PrismaService) {}
 
-  async getUserById(userId: number) {
-    const user = await this.prismaService.user.findUnique({
-      where: { id: userId },
-    });
-
-    if (!user) {
-      throw new UserNotFoundException();
-    }
-
-    return new UserDto(user.id, user.name, user.email);
-  }
-
-  async create(payload: UserDtoPostRequest) {
-    const { password, email } = payload;
-
+  async verifyUserEmail(email: string): Promise<string> {
     const emailExists = await this.prismaService.user.findUnique({
       where: { email },
     });
@@ -35,41 +22,46 @@ export class UserService {
       throw new InvalidEmailException();
     }
 
-    payload.password = await bcrypt.hash(
-      password,
-      AppConfigEnum.BCRYPT_SALT_ROUNDS,
-    );
+    return email;
+  }
+
+  async getUserById(id: number): Promise<UserDto> {
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new UserNotFoundException();
+    }
+
+    return new UserDto(user.id, user.name, user.email);
+  }
+
+  async create(payload: UserDtoPostRequest): Promise<UserDto> {
+    const { password, email } = payload;
+
+    this.verifyUserEmail(email);
+
+    payload.password = await bcrypt.hash(password, AppConfigEnum.BCRYPT_SALT_ROUNDS);
 
     const user = await this.prismaService.user.create({ data: payload });
 
     return new UserDto(user.id, user.name, user.email);
   }
 
-  async update(userId: number, user: UserDtoPutRequest) {
-    const updateUser = await this.prismaService.user.findUnique({
-      where: { id: userId },
-    });
-
-    if (!updateUser) {
-      throw new UserNotFoundException();
-    }
+  async update(userId: number, payload: UserDtoPutRequest): Promise<UserDto> {
+    await this.getUserById(userId);
 
     const updatedUser = await this.prismaService.user.update({
       where: { id: userId },
-      data: user,
+      data: payload,
     });
 
     return new UserDto(updatedUser.id, updatedUser.name, updatedUser.email);
   }
 
-  async delete(userId: number) {
-    const user = await this.prismaService.user.findUnique({
-      where: { id: userId },
-    });
-
-    if (!user) {
-      throw new UserNotFoundException();
-    }
+  async delete(userId: number): Promise<void> {
+    await this.getUserById(userId);
 
     this.prismaService.user.delete({ where: { id: userId } });
   }
