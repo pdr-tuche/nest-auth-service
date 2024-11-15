1f4c3f75f28afbab6085d7366bc20d4e8f6bf089
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-11-14 22:11:39 -0300

    refactor: criado retorno do exception filter e trocado nome da rota

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Post,
   Put,
+  UseFilters,
 } from '@nestjs/common';
 import { UserDtoPostRequest } from '../common/dtos/user/user-dto-post-request.dto';
 import { UserDtoPutRequest } from '../common/dtos/user/user-dto-put-request.dto';
@@ -15,8 +16,10 @@ import { GetUserByIdService } from './providers/get-user-by-id.service';
 import { CreateUserService } from './providers/create-user.service';
 import { UpdateUserService } from './providers/update-user.service';
 import { DeleteUserService } from './providers/delete-user.service';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
-@Controller('auth')
+@Controller('users')
+@UseFilters(HttpExceptionFilter)
 export class UserController {
   constructor(
     private readonly getUserByIdService: GetUserByIdService,
@@ -25,25 +28,25 @@ export class UserController {
     private readonly deleteUserService: DeleteUserService,
   ) {}
 
-  @Get('users/:id')
+  @Get(':id')
   @HttpCode(HttpStatus.OK)
   async show(@Param('id') id: number) {
     return await this.getUserByIdService.handle(id);
   }
 
-  @Post('users')
+  @Post('')
   @HttpCode(HttpStatus.CREATED)
   async store(@Body() payload: UserDtoPostRequest) {
     return await this.createUserService.handle(payload);
   }
 
-  @Put('users/:id')
+  @Put('/:id')
   @HttpCode(HttpStatus.OK)
   async update(@Param('id') id: number, @Body() user: UserDtoPutRequest) {
     return await this.updateUserService.handle(id, user);
   }
 
-  @Delete('users/:id')
+  @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async destroy(@Param('id') id: number) {
     return await this.deleteUserService.handle(id);
